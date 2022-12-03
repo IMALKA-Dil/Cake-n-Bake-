60bb7211b27a1bf3681fe854ddd4f3a4ce2f0db5
IMALKA-Dil <imalkadilshan0910@gmail.com>
2022-12-02 16:31:07 -0800

    this is the day 0 and version 0

---
+++ home.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" type="text/css" href="css\bootstrap-grid.min.css">
+
+      <link rel="stylesheet" type="text/css" href="css\bootstrap.min.css">
+       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+      <link rel="stylesheet" type="text/css" href="css/app.css">
+    <title>Cake n' Bake</title>
+</head>
+<header>
+
+
+  <nav>
+    <ul>
+      <li class="logo">Cake n' Bake</li>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Menu</a></li>
+      <li><a href="#">Specials</a></li>
+      <li><a href="#">Events</a></li>
+      <li><a href="#">Gallery</a></li>
+      <li><a href="#">Contact</a></li>
+      <li><a href="#">WishList</a></li>
+
+
+    </ul>
+  </nav>
+
+
+</header>
+
+<body>
+
+</body>
+</html>
